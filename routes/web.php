bb8dbc6e647a86a471b8b08e7a62b32b90c074a2
MehediHasanRapiIIT <mehedi2515@student.nstu.edu.bd>
2025-11-08 07:39:04 +0600

    registration

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\AccountController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('account/register', [AccountController::class, 'register'])->name('account.register');
+Route::post('account/register', [AccountController::class, 'processRegister'])->name('account.processRegister');
+
+Route::get('account/login', [AccountController::class, 'login'])->name('account.login');
+//Route::post('account/login', [AccountController::class, 'processLogin'])->name('account.processLogin');
