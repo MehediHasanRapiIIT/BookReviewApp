e15700bcb697c32c9e20b10807a38d0fc3ff3c93
MehediHasanRapiIIT <mehedi2515@student.nstu.edu.bd>
2025-11-11 08:08:58 +0600

    Book editing

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
@@ -55,6 +56,7 @@ class BookController extends Controller
         $book->save();
 
         if(!empty($request->image)){
+
             $image = $request->image;
             $ext = $image->getClientOriginalExtension();
             $imageName = time().'.'.$ext;
@@ -73,10 +75,60 @@ class BookController extends Controller
     }
 
     public function edit($id){
+        $book = Book::findOrFail($id);
+        return view('books.edit', [
+            'book'=>$book
+        ]);
 
     }
 
-    public function update(Request $request, $id){
+    public function update($id, Request $request){
+
+        $book = Book::findOrFail($id);
+
+        $rules = [
+            'title'=>'required|min:3|max:255',
+            'author'=>'required|min:3|max:255',
+            'status'=>'required',
+        ];
+
+        if(!empty($request->image)){
+            $rules['image'] = 'image';
+        }
+
+        $vlidator = Validator::make($request->all(), $rules);
+
+        if($vlidator->fails()){
+            return redirect()->route('books.edit',$book->id)->withInput()->withErrors($vlidator);
+        }
+
+        //Update Book in DB
+        $book->title = $request->title;
+        $book->author = $request->author;
+        $book->description = $request->description;
+        $book->status = $request->status;
+        $book->save();
+
+        if(!empty($request->image)){
+
+            File::delete(public_path('uploads/books/'.$book->image));
+            File::delete(public_path('uploads/books/thumb/'.$book->image));
+
+            $image = $request->image;
+            $ext = $image->getClientOriginalExtension();
+            $imageName = time().'.'.$ext;
+            $image->move(public_path('uploads/books/'), $imageName);
+            $book->image = $imageName;
+            $book->save();
+
+            $manager = new ImageManager(new Driver());
+            $img = $manager->read(public_path('uploads/books/' . $imageName));
+            $img->resize(990);
+            $img->save(public_path('uploads/books/thumb/' . $imageName));
+        }
+
+        return redirect()->route('books.index')->with('success', 'Book updated successfully.');
+        
 
     }
 
--- routes/web.php
+++ routes/web.php
@@ -30,5 +30,8 @@ Route::group(['prefix'=>'account'], function(){
         Route::get('books', [BookController::class, 'index'])->name('books.index');
         Route::get('books/create', [BookController::class, 'create'])->name('books.create');
         Route::post('books/store', [BookController::class, 'store'])->name('books.store');
+        Route::get('books/edit/{id}', [BookController::class, 'edit'])->name('books.edit');
+        Route::post('books/edit/{id}', [BookController::class, 'update'])->name('books.update');
+        // Route::get('books/delete/{id}', [BookController::class, 'destroy'])->name('books.destroy');
     });
 });
