1f9fed1b5a20889e9e8b25a55906aced4f46e125
MehediHasanRapiIIT <mehedi2515@student.nstu.edu.bd>
2025-11-13 15:33:01 +0600

    admin middleware for check

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ Route::post('/save-book-review', [HomeController::class, 'saveReview'])->name('b
 
 
 
-Route::group(['prefix'=>'account'], function(){
+Route::group(['prefix' => 'account'], function () {
 
-    Route::group(['middleware'=>'guest'], function(){
+    Route::group(['middleware' => 'guest'], function () {
         // Guest routes go here
         Route::get('register', [AccountController::class, 'register'])->name('account.register');
         Route::post('register', [AccountController::class, 'processRegister'])->name('account.processRegister');
@@ -24,25 +24,30 @@ Route::group(['prefix'=>'account'], function(){
     });
 
 
-    Route::group(['middleware'=>'auth'], function(){
+    Route::group(['middleware' => 'auth'], function () {
         // Protected routes go here
         Route::get('profile', [AccountController::class, 'profile'])->name('account.profile');
         Route::get('logout', [AccountController::class, 'logout'])->name('account.logout');
         Route::post('updateProfile', [AccountController::class, 'updateProfile'])->name('account.updateProfile');
-        Route::get('books', [BookController::class, 'index'])->name('books.index');
-        Route::get('books/create', [BookController::class, 'create'])->name('books.create');
-        Route::post('books/store', [BookController::class, 'store'])->name('books.store');
-        Route::get('books/edit/{id}', [BookController::class, 'edit'])->name('books.edit');
-        Route::post('books/edit/{id}', [BookController::class, 'update'])->name('books.update');
-        Route::delete('books', [BookController::class, 'destroy'])->name('books.destroy');
 
-        Route::get('reviews', [ReviewController::class, 'index'])->name('account.reviews');
-        Route::get('reviews/{id}', [ReviewController::class, 'edit'])->name('account.reviews.edit');
-        Route::post('reviews/{id}', [ReviewController::class, 'updateReview'])->name('account.reviews.updateReview');
-        Route::delete('delete-review', [ReviewController::class, 'deleteReview'])->name('account.reviews.deleteReview');
+        Route::group(['middleware' => 'check-admin'], function () {
+            // Admin only routes go here
+            Route::get('books', [BookController::class, 'index'])->name('books.index');
+            Route::get('books/create', [BookController::class, 'create'])->name('books.create');
+            Route::post('books/store', [BookController::class, 'store'])->name('books.store');
+            Route::get('books/edit/{id}', [BookController::class, 'edit'])->name('books.edit');
+            Route::post('books/edit/{id}', [BookController::class, 'update'])->name('books.update');
+            Route::delete('books', [BookController::class, 'destroy'])->name('books.destroy');
+
+            Route::get('reviews', [ReviewController::class, 'index'])->name('account.reviews');
+            Route::get('reviews/{id}', [ReviewController::class, 'edit'])->name('account.reviews.edit');
+            Route::post('reviews/{id}', [ReviewController::class, 'updateReview'])->name('account.reviews.updateReview');
+            Route::delete('delete-review', [ReviewController::class, 'deleteReview'])->name('account.reviews.deleteReview');
+        });
+
         Route::get('my-reviews', [AccountController::class, 'myReviews'])->name('account.myReviews');
         Route::get('my-review/{id}', [AccountController::class, 'editReview'])->name('account.myReviews.editReview');
         Route::post('my-review/{id}', [AccountController::class, 'updateReview'])->name('account.myReviews.updateReview');
-        Route::delete('delete-my-review', [AccountController::class, 'deleteReview'])->name('account.myReviews.deleteReview'); 
+        Route::delete('delete-my-review', [AccountController::class, 'deleteReview'])->name('account.myReviews.deleteReview');
     });
 });
